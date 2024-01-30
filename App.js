45b648e46fe42fcf5b831a075b42d6ab06fe4c82
BozicDev <bozicdev@gmail.com>
2024-01-30 19:19:39 +0100

    da.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Alerts, CreateList, Home, Profile } from './Screens';
+import { SignIn, SignUp, ResetPassword } from './auth';
 
 export default function App() {
   return (
     <View>
-      
     </View>
   );
 }
