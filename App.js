2c6bed201c9066b96e7399d7d4e1b5a4f5065085
BozicDev <bozicdev@gmail.com>
2024-01-30 16:50:59 +0100

    namjesteni exportovi za screenove

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Alerts, CreateList, Home, Profile } from './Screens';
 
 export default function App() {
   return (
