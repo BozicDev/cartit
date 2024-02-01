51bd69edafc950e9bf0c78bf2cfc2778773ba603
BozicDev <bozicdev@gmail.com>
2024-02-01 20:23:22 +0100

    fixovana navigacija

--- Navigation.js
+++ Navigation.js
@@ -2,22 +2,27 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icons from 'react-native-ionicons'
+import { Home, Alerts, CreateList, Profile } from './Screens'
+import Icon from 'react-native-ionicons'
 
-const home = "Home"
-const alerts = "Alerts"
-const createlist = "Create"
-const profile = "Profile"
 
 const tab = createBottomTabNavigator()
 
-export default function Navigation () {
+export default function Navigation() {
 
     return (
         <NavigationContainer>
             <tab.Navigator
-            initialRouteName={home}>
-                <tab.Screen
+                initialRouteName={Home}
+                screenOptions={{ headerShown: false }}
+                >
+                
+                <tab.Screen name='Home' component={Home} />
+                <tab.Screen name='Create List' component={CreateList} />
+                <tab.Screen name='Alerts' component={Alerts} />
+                <tab.Screen name='Profile' component={Profile} />
             </tab.Navigator>
         </NavigationContainer>
     )
+
 }
