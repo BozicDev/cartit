36be6c7bc2ab1b4cf43b04f7f1fd86f6fca1ab8e
BozicDev <bozicdev@gmail.com>
2024-02-03 00:26:18 +0100

    promjene u navigaciji

--- Navigation.js
+++ Navigation.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Home, Alerts, CreateList, Profile } from './Screens'
+import { Home, Alerts, CreateList, Profile, Settings } from './Screens'
 import { Ionicons, Feather, AntDesign } from '@expo/vector-icons'
 
 
@@ -21,20 +21,24 @@ export default function Navigation() {
                  options={{ tabBarIcon: () => <Ionicons name='home-outline' size={20} /> }} />
 
                 <Tab.Screen 
-                 name='Create List' 
-                 component={CreateList} 
-                 options={{ tabBarIcon: () => <Ionicons name="create-outline" size={20} /> }} />
-
-                <Tab.Screen 
                  name='Alerts' 
                  component={Alerts} 
                  options={{ tabBarIcon: () => <Ionicons name="notifications-outline" size={20} /> }} />
+
+                <Tab.Screen 
+                 name='Create List' 
+                 component={CreateList} 
+                 options={{ tabBarIcon: () => <Ionicons name="create-outline" size={20} /> }} />
 
                 <Tab.Screen 
                  name='Profile' 
                  component={Profile} 
                  options={{ tabBarIcon: () => <AntDesign name='user' size={20} /> }} />
                 
+                <Tab.Screen
+                 name="Settings" 
+                 component={Settings}
+                 options={{tabBarIcon: () => <Ionicons name='settings-outline' size={20}/> }}/>
             </Tab.Navigator>
         </NavigationContainer>
     )
