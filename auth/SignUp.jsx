47bbb1c433a56bd2ef4b4c224ee96b8b65155839
BozicDev <bozicdev@gmail.com>
2024-02-19 10:44:36 +0100

    changes to sign up screen

--- auth/SignUp.jsx
+++ auth/SignUp.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, SafeAreaView } from 'react-native'
+import { View, Text, TextInput, SafeAreaView, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const SignUp = () => {
@@ -7,18 +7,23 @@ const SignUp = () => {
         <View className=" flex-1 justify-center items-center">
             <View className="email-group">
                 <Text> E-Mail</Text>
+                <TextInput/>
             </View>
         </View>
         <View>
             <View className="password-group">
-
+              <Text> Password </Text>
+              <TextInput/>
+              <Text>Ovdje ide za show password btn</Text>
             </View>
         </View>
         <View className="auth-group">
-
+          <Button>Create Account</Button>
+          <Text> Already have an Account? Login here.</Text>
         </View>
         <View className="other-signin-methods">
-
+          <View className="linija"></View>
+          <TouchableOpacity className="google-auth"></TouchableOpacity>
         </View>
     </SafeAreaView>
   )
