b6f6d17736c9c2b68a7f0432ddab91a418b513f2
BozicDev <bozicdev@gmail.com>
2024-02-23 12:55:53 +0100

    layout od auth screenova napravljen

--- auth/SignIn.jsx
+++ auth/SignIn.jsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, SafeAreaView, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const SignIn = () => {
+const SignUp = () => {
   return (
-    <View>
-      <Text className="">SignIn</Text>
-    </View>
+    <SafeAreaView className="pt-10">
+      <View className=" flex-1 justify-center items-center">
+        <View className="email-group">
+          <Text> E-Mail</Text>
+          <TextInput />
+        </View>
+      </View>
+      <View>
+        <View className="password-group">
+          <Text> Password </Text>
+          <TextInput />
+          <Text>Ovdje ide za show password btn</Text>
+        </View>
+      </View>
+      <View className="auth-group">
+        <Button>Sing In</Button>
+        <Text>Don't have an Account? Create here.</Text>
+      </View>
+      <View className="other-signin-methods">
+        <View className="linija"></View>
+        <TouchableOpacity className="google-auth"></TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
-
-export default SignIn
--- auth/SignUp.jsx
+++ auth/SignUp.jsx
@@ -4,27 +4,27 @@ import React from 'react'
 const SignUp = () => {
   return (
     <SafeAreaView className="pt-10">
-        <View className=" flex-1 justify-center items-center">
-            <View className="email-group">
-                <Text> E-Mail</Text>
-                <TextInput/>
-            </View>
+      <View className=" flex-1 justify-center items-center">
+        <View className="email-group">
+          <Text> E-Mail</Text>
+          <TextInput />
         </View>
-        <View>
-            <View className="password-group">
-              <Text> Password </Text>
-              <TextInput/>
-              <Text>Ovdje ide za show password btn</Text>
-            </View>
+      </View>
+      <View>
+        <View className="password-group">
+          <Text> Password </Text>
+          <TextInput />
+          <Text>Ovdje ide za show password btn</Text>
         </View>
-        <View className="auth-group">
-          <Button>Create Account</Button>
-          <Text> Already have an Account? Login here.</Text>
-        </View>
-        <View className="other-signin-methods">
-          <View className="linija"></View>
-          <TouchableOpacity className="google-auth"></TouchableOpacity>
-        </View>
+      </View>
+      <View className="auth-group">
+        <Button>Create Account</Button>
+        <Text> Already have an Account? Login here.</Text>
+      </View>
+      <View className="other-signin-methods">
+        <View className="linija"></View>
+        <TouchableOpacity className="google-auth"></TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
